5aec122d18449da05ee819999c03b6206095b4b7
lopez1k <a.pushchak08@gmail.com>
2025-04-06 21:36:43 +0300

    new

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, redirect, render_template
+import requests
+
+app = Flask(__name__)
+
+CHANNEL_LINK = "https://t.me/MyTokenCommunity"
+
+def is_uk_ip(ip):
+    try:
+        response = requests.get(f"http://ip-api.com/json/{ip}")
+        data = response.json()
+        print(data)
+        return data.get("countryCode") == "GB"
+    except Exception as e:
+        return False  # Якщо не вдалося визначити — допускаємо
+
+@app.route("/")
+def verify():
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0]
+    print(ip)
+    if is_uk_ip(ip):
+        return render_template('index.html', user_ip = ip)
+    else:
+        return redirect('https://t.me/pscommunity24')
+
+@app.route("/redirect_page")
+def redircet():
+    return redirect('https://t.me/pscommunity24')
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
